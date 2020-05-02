0c5cfd530daa53f38a51a279dcb393d9f16cbc0f
Brent V. Bingham <bvbgrad@gmail.com>
2020-05-02 08:59:43 -0500

    Initial commit

---
+++ catgui.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+import PySimpleGUI as sg
+
+from pathlib import Path
+
+
+def main():
+    args = getargs()
+
+    script_name = Path(sys.argv[0]).stem
+    options_text = f"{script_name} options:\n  {args}"
+    sg.popup(options_text, title=script_name)
+
+
+def getargs():
+    parser = argparse.ArgumentParser(
+        description="A collection of disk file cataloging functions")
+    parser.add_argument('-v', '--verbose', help='Provide detailed information')
+    parser.add_argument('--version', action='version', version='%(prog)s version 0.1')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    main()
