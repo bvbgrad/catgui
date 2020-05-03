61add943cc739eeaf51393a5e54bf2bd6a7ad87d
Brent V. Bingham <bvbgrad@gmail.com>
2020-05-03 02:35:58 -0500

    Add date picker

--- catgui.py
+++ catgui.py
@@ -1,5 +1,5 @@
-__author__ = 'Brent V. Bingham'
-__version__ = '0.1'
+author = __author__ = 'Brent V. Bingham'
+version = __version__ = '0.1'
 import argparse
 
 from catguiAuxPkg import catguiMenu
@@ -8,7 +8,7 @@ from catguiAuxPkg import catguiMenu
 def main():
     args = getargs()
 
-    catguiMenu.menu(__author__, __version__, args)
+    catguiMenu.menu(author, version, args)
 
 
 def getargs():
@@ -16,7 +16,7 @@ def getargs():
         description="A collection of disk file cataloging functions")
     parser.add_argument('-v', '--verbose', default=False,
         help='Provide detailed information')
-    parser.add_argument('--version', action='version', version='%(prog)s {__version__}')
+    parser.add_argument('--version', action='version', version='%(prog)s {version}')
     args = parser.parse_args()
     return args
 
--- catguiAuxPkg/catguiMenu.py
+++ catguiAuxPkg/catguiMenu.py
@@ -1,10 +1,11 @@
+import datetime
 from pathlib import Path
 import sys
 
 import PySimpleGUI as sg
 
 
-def menu(__author__, __version__, args):
+def menu(author, version, args):
     
     sg.ChangeLookAndFeel('LightGreen')
     sg.SetOptions(element_padding=(0, 0))
@@ -32,11 +33,28 @@ def menu(__author__, __version__, args):
         print('Button = ', event)
         # ------ Process menu choices ------ #
         if event == 'About...':
-            about_text = f"{script_name} v{__version__} (c) 2020\nAuthor: {__author__}\nOptions:" \
+            about_text = f"{script_name} v{version} (c) 2020\nAuthor: {author}\nOptions:" \
                 f"\n  {args}"
             sg.popup(about_text, title=script_name)
         elif event == 'Open':
             folder_name = sg.popup_get_folder('Starting folder', no_window=True)
             print(folder_name)
+        elif event == 'Since...':
+            date30 = datetime.datetime.today() - datetime.timedelta(days=30)
+            date = date30.strftime("%Y-%m-%d")
+            layout = [[sg.In(date, size=(20,1), key='input')],
+                    [sg.CalendarButton('Choose Date', target='input', key='date', 
+                        default_date_m_d_y=(date30.month, date30.day, date30.year),
+                        button_color=('black','lightblue'), 
+                        tooltip='Default is 30 days prior to today.')],
+                    [sg.Ok(key='Ok')]]
+
+            calendar_window = sg.Window('Calendar', grab_anywhere=False, ).Layout(layout)
+            calendar_event,calendar_values = calendar_window.Read()
+            if calendar_event == 'Ok':
+                date = calendar_values['input'][:10]
+                calendar_window.close() 
+            print(date)
+
 
     window.close()
